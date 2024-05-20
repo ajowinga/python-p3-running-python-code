1d0920bd2c8cc69897746f5d7bd4f5e5cf999334
ajowinga <joanwinga1@gmail.com>
2024-05-21 00:18:44 +0300

    done well

---
+++ lib/app.py
@@ -0,0 +1,7 @@
+# print("Hello world!", end=" ")
+# print("Hello sun!", end="!! ")
+# print("Hello sky!", end="!!!")
+
+print("Hello World! Pass this test, please." , end="\n")
+# print("Hello World! Pass this test, please.")
+# print('Hello world!...st, please.\n' == 'Hello World!...st, please.\n')
